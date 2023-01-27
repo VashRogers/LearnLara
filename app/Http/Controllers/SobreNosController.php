99eb9e98c956fe517d320c1cccd3aeb215b6f7ef
VashRogers <rogerssj@gmail.com>
2023-01-27 13:33:44 -0400

    Correção no caminho da view sobre

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SobreNosController extends Controller
+class SobreNosController extends Controller 
 {
     //
     public function sobre() {
