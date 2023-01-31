06857efab404e80f2f325464ab656af9118d080e
VashRogers <rogerssj@gmail.com>
2023-01-31 16:30:02 -0400

    Usando create e fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     //
-    protected $table = "fornecedores";//Corrigindo erro de busca no banco com nome fornecedors
-    
+    protected $table = "fornecedores";  //Corrigindo erro de busca no banco com nome fornecedors
+    protected $fillable = ['nome', 'site', 'uf', 'email']; //define os campos a serem registrados no campo
 
 }
