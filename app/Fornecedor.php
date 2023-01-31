8b82341b1ae81010c26a1d59f7b7c354afd3446f
VashRogers <rogerssj@gmail.com>
2023-01-31 16:11:02 -0400

    Adicionando atributo table indicando nome da tabela

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     //
+    protected $table = "fornecedores";//Corrigindo erro de busca no banco com nome fornecedors
+    
+
 }
