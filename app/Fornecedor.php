6b1adb1195c89830b232336b14665fa89b8cc556
VashRogers <rogerssj@gmail.com>
2023-02-01 07:58:25 -0400

    Comentando sobre metodos do Eloquent(Estão no model Fornecedores)

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -9,5 +9,6 @@ class Fornecedor extends Model
     //
     protected $table = "fornecedores";  //Corrigindo erro de busca no banco com nome fornecedors
     protected $fillable = ['nome', 'site', 'uf', 'email']; //define os campos a serem registrados no campo
-
+    //método save() serve para inserir registros;
+    //método create([vai um array de objetos]); faz uma inserção no banco;
 }
