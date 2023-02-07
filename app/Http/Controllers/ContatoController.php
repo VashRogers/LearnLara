dab824e86044185cd2ff524ce43ebfebfafe6f83
VashRogers <rogerssj@gmail.com>
2023-02-07 08:31:03 -0400

    Validações de formulário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -19,12 +19,21 @@ class ContatoController extends Controller
     public function salvar(Request $request) {
         // SiteContato::create($request->all());
         $request->validate([
-            'nome' => 'required|min:3|max:40',//qtd min e max de caracteres
+            'nome' => 'required|min:3|max:40|unique:site_contatos',//qtd min e max de caracteres
             'telefone'=>'required',
             'email' => 'email',
             'motivo_contatos_id'=>'required',
             'mensagem'=>'required|max:200'
-        ]);
+        ], 
+        [
+            'nome.required'=>'É necessário digitar um nome',
+            'nome.min' => 'É preciso no minimo 3 caracteres para nome',
+            'telefone.required' => 'Digite seu telefone',
+            'email.email'=>'Insira seu email',
+            'motivo_contatos_id.required'=>'É necessário dar um motivo',
+            'mensagem.required'=>'É obrigatório digitar uma mensagem'
+        ]
+    );
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,12 +1,17 @@
 
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
+
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('nome'))
+        {{ $errors->first('nome') }}
+    @endif
     <br>
     <input name="telefone" value="{{ old('telefone') }}"  type="text" placeholder="Telefone" class="{{ $classe }}">
+        {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
     <input name="email" value="{{ old('email') }}"  type="text" placeholder="E-mail" class="{{ $classe }}">
-    
+        {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
 
     <select name="motivo_contatos_id" class="{{ $classe }}">
@@ -16,7 +21,7 @@
             <option value="{{ $motivo_contato->id }}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>{{ $motivo_contato->motivo_contato }}</option>
         @endforeach
     </select>
-
+        {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
         {{-- <option value="1" {{ old('motivo_contato') == 1 ? 'selected' : '' }}>Dúvida</option>
         <option value="2" {{ old('motivo_contato') == 2 ? 'selected' : '' }}>Elogio</option>
         <option value="3" {{ old('motivo_contato') == 3 ? 'selected' : '' }}>Reclamação</option> --}}
@@ -30,13 +35,16 @@
             Preencha aqui su message
         @endif
     </textarea>
+        {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-
-{{-- <div style="position: absolute; top:0px; left:0px; width:100%; background:red;" >
-    <pre>
-        {{ print_r($errors) }}
-    </pre>
-</div> --}}
+{{-- @if($errors->any())
+    <div style="position: absolute; top:0px; left:0px; width:100%; background:red;" >
+        @foreach ($errors->all() as $erro )    
+            {{ $erro }}
+            <br>
+        @endforeach
+    </div>
+@endif --}}
