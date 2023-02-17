fca70256a5c4afc1202669679b262d01d182de2e
VashRogers <rogerssj@gmail.com>
2023-02-17 13:07:14 -0400

    Checkpoint 2

--- app/Item.php
+++ app/Item.php
@@ -9,7 +9,7 @@ class Item extends Model
     protected $table = 'produtos';
     protected $fillable = [ 'nome', 'descricao', 'peso', 'unidade_id' ];
 
-    public function produtoDetalhe () {
+    public function itemDetalhe () {
         return $this->hasOne('App\ItemDetalhe', 'produto_id', 'id');//Produto tem 1 produtoDetalhe, rel 1x1;
     }
 }
